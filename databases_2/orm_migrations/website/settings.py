bb35747f68914e4e04f865a5269b30e65a7f223e
Alexander Tsupko <alexander.tsupko@outlook.com>
2026-02-26 18:17:17 +0300

    Add Django Debug Toolbar

--- databases_2/orm_migrations/website/settings.py
+++ databases_2/orm_migrations/website/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'school',
 ]
 
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
 ]
 
 ROOT_URLCONF = 'website.urls'
@@ -125,6 +127,8 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+INTERNAL_IPS = ['127.0.0.1']
 
 try:
     from .settings_local import *
