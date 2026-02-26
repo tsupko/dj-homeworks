038a301c0620d41d60d3ea59d8de0f2543abbb6f
Alexander Tsupko <alexander.tsupko@outlook.com>
2026-02-26 18:12:00 +0300

    Add ordering

--- databases_2/orm_migrations/school/views.py
+++ databases_2/orm_migrations/school/views.py
@@ -4,12 +4,12 @@ from .models import Student
 
 
 def students_list(request):
-    template = 'school/students_list.html'
-    students = Student.objects.all()
-    context = {'object_list': students}
-
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = 'group'
+
+    template = 'school/students_list.html'
+    students = Student.objects.all().order_by(ordering)
+    context = {'students': students}
 
     return render(request, template, context)
