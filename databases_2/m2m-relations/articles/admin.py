d33a8af541a1f18cb2c3f3ee706c42a34f156852
Alexander Tsupko <alexander.tsupko@outlook.com>
2026-02-27 18:31:46 +0300

    Add inline for many-to-many relationship at the admin panel

--- databases_2/m2m-relations/articles/admin.py
+++ databases_2/m2m-relations/articles/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Article, Tag
+from .models import Article, Tag, Relationship
 
 
 @admin.register(Article)
@@ -8,6 +8,11 @@ class ArticleAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'text', 'published_at', 'image']
 
 
+class RelationshipInline(admin.TabularInline):
+    model = Relationship
+
+
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ['name']
+    inlines = [RelationshipInline]
