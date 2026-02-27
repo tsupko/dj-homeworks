b683c401fc43bcfaeea872f944e3b3f1ee4e362f
Alexander Tsupko <alexander.tsupko@outlook.com>
2026-02-27 19:09:44 +0300

    Move many-to-many relationship to the Article model

--- databases_2/m2m-relations/articles/models.py
+++ databases_2/m2m-relations/articles/models.py
@@ -1,23 +1,8 @@
 from django.db import models
 
 
-class Article(models.Model):
-    title = models.CharField(max_length=256, verbose_name='Название')
-    text = models.TextField(verbose_name='Текст')
-    published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
-
-    class Meta:
-        verbose_name = 'Статья'
-        verbose_name_plural = 'Статьи'
-
-    def __str__(self):
-        return self.title
-
-
 class Tag(models.Model):
     name = models.CharField(max_length=256, verbose_name='Название')
-    articles = models.ManyToManyField(Article, related_name='tags', through='Relationship')
 
     class Meta:
         verbose_name = 'Раздел'
@@ -27,10 +12,25 @@ class Tag(models.Model):
         return self.name
 
 
+class Article(models.Model):
+    title = models.CharField(max_length=256, verbose_name='Название')
+    text = models.TextField(verbose_name='Текст')
+    published_at = models.DateTimeField(verbose_name='Дата публикации')
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение')
+    tags = models.ManyToManyField(Tag, related_name='articles', through='ArticleTag', verbose_name='Разделы')
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+
+    def __str__(self):
+        return self.title
+
+
 class ArticleTag(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    is_main_tag = models.BooleanField(default=False)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, verbose_name='Статья')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name='Раздел')
+    is_main_tag = models.BooleanField(default=False, verbose_name='Основной')
 
     class Meta:
         unique_together = ('article', 'tag')
