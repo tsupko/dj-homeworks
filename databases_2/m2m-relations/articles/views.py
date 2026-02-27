ef858a07f591ca357b4b79706a43cd68f22e5b5a
Alexander Tsupko <alexander.tsupko@outlook.com>
2026-02-27 18:07:18 +0300

    Pass articles to context

--- databases_2/m2m-relations/articles/views.py
+++ databases_2/m2m-relations/articles/views.py
@@ -1,12 +1,12 @@
-from django.views.generic import ListView
 from django.shortcuts import render
 
-from articles.models import Article
+from .models import Article
 
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {}
+    articles = Article.objects.all()
+    context = {'object_list': articles}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
