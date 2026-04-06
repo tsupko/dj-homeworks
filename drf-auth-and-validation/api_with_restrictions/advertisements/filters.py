b35f93278365f5f1d726626b56c4d30035539ba7
Alexander Tsupko <alexander.tsupko@outlook.com>
2026-04-06 18:19:49 +0300

    Add basic filtration using `django-filter`

--- drf-auth-and-validation/api_with_restrictions/advertisements/filters.py
+++ drf-auth-and-validation/api_with_restrictions/advertisements/filters.py
@@ -1,12 +1,19 @@
-from django_filters import rest_framework as filters
-
+import django_filters as filters
 from advertisements.models import Advertisement
 
 
 class AdvertisementFilter(filters.FilterSet):
     """Фильтры для объявлений."""
+    # Фильтр для даты (диапазон "от" и "до")
+    created_at = filters.DateFromToRangeFilter(field_name='created_at')
 
-    # TODO: задайте требуемые фильтры
+    # Фильтр для статуса (точное совпадение)
+    status = filters.CharFilter(field_name='status')
+
+    # Фильтр для создателя (по ID пользователя)
+    creator = filters.NumberFilter(field_name='creator')
 
     class Meta:
         model = Advertisement
+        # Указываем все поля, по которым будет возможна фильтрация. Это активирует фильтры.
+        fields = ['created_at', 'status', 'creator']
